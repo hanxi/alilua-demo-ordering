e2af20ba5c2cc90453f44fc19261e6f2f8856b06
hanxi <me@hanxi.info>
2015-03-04 15:03:07 +0800

    完善目录结构

--- index.lua
+++
@@ -1,7 +0,0 @@
-echo(__root)
-local sock = cosocket.tcp()
-local host = "192.168.16.234"
-local port = 8989
-local r,e = sock:connect(host,port)
-r,e =  sock:send("hello")
-rts, err = dotemplate("/index.html")
---
+++ restart.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+filepath=$(cd "$(dirname "$0")"; pwd)
+
+stop()
+{
+    pid=`ps -ef| grep "alilua" | grep -v grep | awk '{print $2}' `
+    if [ "$pid"x != x ]; then
+        kill -9 $pid
+        echo "stop alilua"
+    fi
+}
+
+start()
+{
+    echo "start alilua prot:9999"
+    $filepath/alilua/alilua process=4 bind=9999 daemon host-route=$filepath/host-route.lua
+    echo "ok"
+}
+
+stop
+start
+
